9be394c72febf006978bdfd00f820f20e89c05b8
cmerrill01 <chasejm@byu.edu>
2024-02-22 15:54:03 -0700

    enhance(gameplay.js) finish Game and DemandCurve constructors

--- gameplay.js
+++ gameplay.js
@@ -1,16 +1,23 @@
+const maxFixedCost = 1000;
+const maxVariableCost = 100;
+const maxInterceptAboveFC = 1000;
+const maxSlope = 100;
+
 class Game {
     assets;
     month;
-    demandCurve;
     fixedCost;
     variableCost;
+    demandCurve;
 
     constructor() {
         this.assets = 0;
         this.month = 1;
-        this.demandCurve = new DemandCurve();
         // generate random fixed cost
+        this.fixedCost = Math.ceil(Math.random() * maxFixedCost);
         // generate random variable cost
+        this.variableCost = Math.ceil(Math.random() * maxVariableCost);
+        this.demandCurve = new DemandCurve(this.fixedCost);
     }
 }
 
@@ -20,10 +27,14 @@ class DemandCurve {
     minRecommendedQuantity;
     maxRecommendedQuantity;
 
-    constructor() {
+    constructor(fixedCost) {
         // generate a random intercept
+        this.intercept = fixedCost + Math.ceil(Math.random() * maxInterceptAboveFC);
         // generate a random slope
+        this.slope = -1 * Math.ceil(Math.random() * (this.intercept - fixedCost < maxSlope ? this.intercept - fixedCost : maxSlope));
         // recommend min and max quantity
         // based on the generated slope and intercept
+        this.minRecommendedQuantity = Math.floor((this.intercept / this.slope) * -0.1);
+        this.maxRecommendedQuantity = Math.ceil((this.intercept / this.slope) * -0.9);
     }
 }
