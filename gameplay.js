175a163fc07fbef91dd7a670fea0140304b44c8e
cmerrill01 <chasejm@byu.edu>
2024-02-27 12:24:56 -0700

    update(gameplay.js) implement calculateRevenue

--- gameplay.js
+++ gameplay.js
@@ -1,6 +1,6 @@
 const maxFixedCost = 1000;
 const maxVariableCost = 100;
-const maxInterceptAboveFC = 1000;
+const maxIntercept = 1000;
 const maxSlope = 100;
 
 class Game {
@@ -17,7 +17,21 @@ class Game {
         this.fixedCost = Math.ceil(Math.random() * maxFixedCost);
         // generate random variable cost
         this.variableCost = Math.ceil(Math.random() * maxVariableCost);
-        this.demandCurve = new DemandCurve(this.fixedCost);
+        this.demandCurve = new DemandCurve();
+    }
+
+    submitPriceAndQuantity(price, quantity) {
+        // Calculate revenue
+        // Calculate cost
+        // Calculate profit
+        // Update gameplay data
+    }
+
+    calculateRevenue(price, quantity) {
+        // Check whether the market will buy this quantity or less at this price
+        const quantityDemanded = Math.round((price - this.demandCurve.intercept) / this.demandCurve.slope);
+        const quantitySold = (quantity <= quantityDemanded ? quantity : quantityDemanded);
+        return price * quantitySold;
     }
 }
 
@@ -27,14 +41,28 @@ class DemandCurve {
     minRecommendedQuantity;
     maxRecommendedQuantity;
 
-    constructor(fixedCost) {
+    constructor() {
         // generate a random intercept
-        this.intercept = fixedCost + Math.ceil(Math.random() * maxInterceptAboveFC);
+        this.intercept = Math.ceil(Math.random() * maxIntercept);
         // generate a random slope
-        this.slope = -1 * Math.ceil(Math.random() * (this.intercept - fixedCost < maxSlope ? this.intercept - fixedCost : maxSlope));
+        this.slope = -1 * Math.ceil(Math.random() * maxSlope);
         // recommend min and max quantity
         // based on the generated slope and intercept
         this.minRecommendedQuantity = Math.floor((this.intercept / this.slope) * -0.1);
         this.maxRecommendedQuantity = Math.ceil((this.intercept / this.slope) * -0.9);
     }
 }
+
+
+
+// Testing
+const game = new Game();
+console.log("assets: " + game.assets);
+console.log("month: " + game.month);
+console.log("fc: " + game.fixedCost);
+console.log("vc: " + game.variableCost);
+const curve = game.demandCurve;
+console.log("intercept: " + curve.intercept);
+console.log("slope: " + curve.slope);
+console.log("minQ: " + curve.minRecommendedQuantity);
+console.log("maxQ: " + curve.maxRecommendedQuantity);
