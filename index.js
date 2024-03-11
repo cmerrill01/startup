4f6908f6455ea728fd7c2ccaf1b68d0ed5bcb078
cmerrill01 <chasejm@byu.edu>
2024-03-11 13:15:15 -0600

    add(public) and add appropriate files to this directory

--- index.js
+++
@@ -1,9 +0,0 @@
-function showUsername() {
-    const username = localStorage.getItem("username");
-    if (username !== null) {
-        navLoginEl = document.querySelector("#nav-login");
-        navLoginEl.textContent = "Welcome, " + username;
-    }   
-}
-
-showUsername();
