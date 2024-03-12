0c630b8513019e8417de1088b5fa896ab52c10a7
cmerrill01 <chasejm@byu.edu>
2024-03-11 18:36:52 -0600

    update(instructions.html, instructions.js) display random quote and author

--- public/instructions.js
+++ public/instructions.js
@@ -2,8 +2,17 @@ function getRandomBusinessQuote() {
     fetch('https://api.quotable.io/quotes/random?tags=business')
     .then((response) => response.json())
     .then((data) => {
-        console.log("author: " + data.author);
-        console.log("quote: " + data.author);
+        console.log(data);
+
+        console.log("author: " + data[0].author);
+        const author = data[0].author;
+        const authorEl = document.querySelector("#quote-author");
+        authorEl.textContent = author;
+
+        console.log("quote: " + data[0].content);
+        const quote = data[0].content;
+        const quoteEl = document.querySelector("#quote-content");
+        quoteEl.textContent = quote;
     });
 }
 
