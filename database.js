6d3390f5784d5496386d39dbf393b56db005393d
cmerrill01 <chasejm@byu.edu>
2024-03-15 15:38:25 -0600

    update(database.js) to include a function that tests whether we have successfully connected to the database

--- database.js
+++ database.js
@@ -1,2 +1,19 @@
+const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
+
 const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+
+async function testConnection() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("sample_mflix").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  testConnection().catch(console.dir);
