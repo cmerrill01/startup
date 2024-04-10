67da3e2f19d5bad1b51ac360e0eb9f9b93abc64d
cmerrill01 <chasejm@byu.edu>
2024-04-10 17:34:19 -0600

    update(src) add stubs for react components

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,45 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Gameplay } from './gameplay/gameplay';
+import { Scores } from './scores/scores';
+import { Instructions } from './instructions/instructions';
 
 export default function App() {
     return (
-        <div className='body'>
-            <header className="container-fluid fixed-top bg-primary text-light">
-                <nav className="navbar navbar-light text-center">
-                    <a className="navbar-brand" href="index.html">EconoMentor</a>
-                    <menu className="navbar-nav">
-                        <li className="nav-item"><a className="nav-link" href="instructions.html">How to Play</a></li>
-                        <li className="nav-item"><a className="nav-link" href="gameplay.html">New Game</a></li>
-                        <li className="nav-item"><a className="nav-link" href="scores.html">Scores</a></li>
-                        <li className="nav-item"><a className="nav-link" href="index.html" id="nav-login">Log In</a></li>
-                    </menu>
-                </nav>
-            </header>
+        <BrowserRouter>
+            <div className='body'>
+                <header className="container-fluid fixed-top bg-primary text-light">
+                    <nav className="navbar navbar-light text-center">
+                        <NavLink className="navbar-brand" to="">EconoMentor</NavLink>
+                        <menu className="navbar-nav">
+                            <li className="nav-item"><NavLink className="nav-link" to="instructions">How to Play</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" to="gameplay">New Game</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" to="scores">Scores</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" to="" id="nav-login">Log In</NavLink></li>
+                        </menu>
+                    </nav>
+                </header>
 
-            <main>App components go here</main>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/gameplay' element={<Gameplay />} />
+                    <Route path='/scores' element={<Scores />} />
+                    <Route path='/instructions' element={<Instructions />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-            <footer className="bg-dark text-light fixed-bottom">
-                <div className="author-name">EconoMentor was created by Chase Merrill at Brigham Young University for C S 260: Web Programming, Winter 2024</div>
-                <a className="footer-link" href="https://github.com/cmerrill01/startup">Source code on GitHub</a>
-            </footer>
-        </div>
+                <footer className="bg-dark text-light fixed-bottom">
+                    <div className="author-name">EconoMentor was created by Chase Merrill at Brigham Young University for C S 260: Web Programming, Winter 2024</div>
+                    <a className="footer-link" href="https://github.com/cmerrill01/startup">Source code on GitHub</a>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
+}
+
+function NotFound() {
+    return <main className='container-fluid text-center'>404: Return to sender. Address unknown.</main>;
 }
---
+++ src/instructions/instructions.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Instructions() {
+  return (
+    <main className='container-fluid text-center'>
+      <div>instructions displayed here</div>
+    </main>
+  );
+}
