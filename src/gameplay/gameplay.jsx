b5758d9e1e1b4fd434e8122e1fe63b4ddd6a527f
cmerrill01 <chasejm@byu.edu>
2024-04-15 16:31:04 -0600

    update(gameplay) render last month's data in Game component

--- src/gameplay/gameplay.jsx
+++ src/gameplay/gameplay.jsx
@@ -5,18 +5,6 @@ export function Gameplay() {
     return (
         <main className="container-fluid bg-light text-dark gameplay-screen">
             <Game maxFixedCost={1000} maxVariableCost={100} maxIntercept={1000} maxSlope={100} maxMonths={12} />
-            <section id="last-round-result" className="gameplay-section">
-                <h1>Last Month's Data</h1>
-                <div id="last-month-revenue-display" className="gameplay-data revenue">
-                    Revenue: $<span id="last-month-revenue">-</span>
-                </div>
-                <div id="last-month-costs-display" className="gameplay-data costs">
-                    Costs: $<span id="last-month-costs">-</span>
-                </div>
-                <div id="last-month-profit-display" className="gameplay-data profit">
-                    Profit: $<span id="last-month-profit">-</span>
-                </div>
-            </section>
             <section id="user-input" className="gameplay-section">
                 <h1>Input Price and Quantity</h1>
                 <input
@@ -71,12 +59,20 @@ class DemandCurve {
 
 function Game({ maxFixedCost, maxVariableCost, maxIntercept, maxSlope, maxMonths }) {
     const gameId = generateUUID();
+    const demandCurve = new DemandCurve(maxIntercept, maxSlope);
+    const [gameOver, setGameOver] = React.useState(false);
+
     const [assets, setAssets] = React.useState(0);
     const [month, setMonth] = React.useState(1);
     const fixedCost = Math.ceil(Math.random() * maxFixedCost);
     const variableCost = Math.ceil(Math.random() * maxVariableCost);
-    const demandCurve = new DemandCurve(maxIntercept, maxSlope);
-    const [gameOver, setGameOver] = React.useState(false);
+
+    const [revenue, setRevenue] = React.useState(null);
+    const [cost, setCost] = React.useState(null);
+    const [profit, setProfit] = React.useState(null);
+
+    const [price, setPrice] = React.useState(null);
+    const [quantity, setQuantity] = React.useState(null);
 
     function generateUUID() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -86,21 +82,51 @@ function Game({ maxFixedCost, maxVariableCost, maxIntercept, maxSlope, maxMonths
         });
     }
 
+    function calculateRevenue(price, quantity) {
+        // Check whether the market will buy this quantity or less at this price
+        const quantityDemanded = Math.round((price - this.demandCurve.intercept) / this.demandCurve.slope);
+        const quantitySold = (quantity <= quantityDemanded ? quantity : quantityDemanded);
+        return price * quantitySold;
+    }
+
+    function calculateCost(quantity) {
+        return this.fixedCost + this.variableCost * quantity;
+    }
+
+    function calculateProfit(revenue, cost) {
+        return revenue - cost;
+    }
+
+    function submit() {
+        if (gameOver === false) {
+
+        }
+    }
+
     return (
-        <section className="gameplay-section">
-            <h1>Gameplay Data</h1>
-            <GameplayData label={"Current assets: $"} value={assets} />
-            <GameplayData label={"Current month: "} value={month} />
-            <GameplayData label={"Recommended quantity: "} value={String(demandCurve.minRecommendedQuantity + " - " + demandCurve.maxRecommendedQuantity)} />
-            <GameplayData label={"Fixed cost: $"} value={fixedCost} />
-            <GameplayData label={"Variable cost per unit: $"} value={variableCost} />
-        </section>
+        <>
+            <section className="gameplay-section">
+                <h1>Gameplay Data</h1>
+                <GameplayData label={"Current assets: $"} value={assets} />
+                <GameplayData label={"Current month: "} value={month} />
+                <GameplayData label={"Recommended quantity: "} value={String(demandCurve.minRecommendedQuantity + " - " + demandCurve.maxRecommendedQuantity)} />
+                <GameplayData label={"Fixed cost: $"} value={fixedCost} />
+                <GameplayData label={"Variable cost per unit: $"} value={variableCost} />
+            </section>
+            <section className="gameplay-section">
+                <h1>Last Month's Data</h1>
+                <GameplayData label={"Revenue: $"} value={revenue} additionalClass={"revenue"} />
+                <GameplayData label={"Costs: $"} value={cost} additionalClass={"costs"} />
+                <GameplayData label={"Profit: $"} value={profit} additionalClass={"profit"} />
+            </section>
+        </>
     )
 }
 
-function GameplayData({label, value}) {
+function GameplayData({ label, value, additionalClass }) {
+    const _className = String("gameplay-data" + (additionalClass ? " " + additionalClass : ""));
     return (
-        <div className="gameplay-data">
+        <div className={_className}>
             {label}<span>{value}</span>
         </div>
     )
